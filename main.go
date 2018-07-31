265ddc4c8994f0a6f24073b3aa9bc06ee42d31c2
kirkbyers <kirklbyers@gmail.com>
2018-07-31 13:14:20 -0500

    Serve WS hub

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/http"
+
+	"github.com/kirkbyers/openstall-master/sockets"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	port := flag.Int("port", 4567, "port to start server on")
+	flag.Parse()
+
+	// Init sockets before http.ListenAndServe
+	sockets.Init()
+
+	http.HandleFunc("/ws", sockets.WebsocketHTTPHandler)
+
+	fmt.Printf("Server Listening on port %v\n", *port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", *port), nil)
+	if err != nil {
+		fmt.Println("There was a problem listing and serving", err)
+	}
 }
