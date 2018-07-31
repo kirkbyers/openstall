4bb728f5ea5cd6c6b0384666ff70f3b7f123ab16
kirkbyers <kirklbyers@gmail.com>
2018-07-31 13:24:59 -0500

    Copy chat example

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import (
 	"github.com/kirkbyers/openstall-master/sockets"
 )
 
+func serveChatExample(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "chat-example.html")
+}
+
 func main() {
 	port := flag.Int("port", 4567, "port to start server on")
 	flag.Parse()
@@ -15,6 +28,7 @@ func main() {
 	// Init sockets before http.ListenAndServe
 	sockets.Init()
 
+	http.HandleFunc("/", serveChatExample)
 	http.HandleFunc("/ws", sockets.WebsocketHTTPHandler)
 
 	fmt.Printf("Server Listening on port %v\n", *port)
