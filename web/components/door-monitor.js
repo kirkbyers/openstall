d525b1f5b47ce6fc78ea53d93f57cede7a1e03e0
kirkbyers <kirklbyers@gmail.com>
2018-08-04 15:43:10 -0500

    Get and update all active monitors in real time on web UI

--- web/components/door-monitor.js
+++ web/components/door-monitor.js
@@ -1,21 +1,83 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { icon } from '@fortawesome/fontawesome-svg-core'
 import { faDoorOpen, faDoorClosed } from '@fortawesome/free-solid-svg-icons'
+
+function updateMonitor(newMonitor, monitorArray) {
+    const result = monitorArray.slice(0);
+    let found = -1;
+    for (let i = 0; i < monitorArray.length; i++) {
+        if (newMonitor.id == monitorArray[i].id) {
+            result[i] = newMonitor;
+            found = i;
+            break
+        }
+    }
+    if (found < 0) {
+        result.push(newMonitor)
+    }
+    return result
+}
+
+function getMonitors() {
+    return fetch(`//${process.env.API_BASE}/status`)
+}
+
+export class DoorMonitors extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monitors: [],
+            wsConn: new WebSocket(`ws://${process.env.API_BASE}/sub`),
+            wsConnected: true
+        };
+        this.state.wsConn.onclose = (evt) => {
+            this.setState(() => ({ wsConnected: false }));
+        };
+        this.state.wsConn.onmessage = (evt) => {
+            var messages = evt.data.split('\n');
+            for (var i = 0; i < messages.length; i++) {
+                const parsedMonitor = JSON.parse(messages[i]);
+                this.setState(() => ({ monitors: updateMonitor(parsedMonitor, this.state.monitors) }));
+            }
+        }
+        getMonitors().then((res) => {
+            return res.json()
+        }).then((resJson) => {
+            this.setState(() => ({ monitors: resJson }))
+        });
+    }
+    render(props, state) {
+        return (
+            <div>
+                {state.monitors.map(((monitor) => {
+                    return (
+                        <DoorMonitor monitor={monitor} />
+                    )
+                }))}
+            </div>
+        )
+    }
+}
 
 export const DoorMonitor = (props) => {
     const iconConfig = {
         transform: {
-            // size: 50
         },
         styles: { 'background-color': 'white', 'color': 'blue' },
-        // classes: ['fa-5x']
     };
-
+    const { name, status } = props.monitor;
     const openDoorIcon = icon(faDoorOpen, iconConfig);
-    const closedDoorIcon = icon({ iconName: 'door-closed', transform: iconConfig });
+    const closedDoorIcon = icon(faDoorClosed, iconConfig);
+    const activeIcon = status === 'open' ? openDoorIcon : closedDoorIcon;
     return (
         <div>
-            This is a door <span dangerouslySetInnerHTML={{ __html: openDoorIcon.html }} />
+            <h3>
+                {name}
+            </h3>
+            <div dangerouslySetInnerHTML={{ __html: activeIcon.html }} />
+            <p>
+                is <b><i>{status}</i></b>.
+            </p>
         </div>
     )
 }
--- web/index.js
+++ web/index.js
@@ -1,35 +1,9 @@
 import { h, render } from 'preact';
 
-import { DoorMonitor } from './components'
+import { DoorMonitors } from './components'
 
 render((
     <div id="app">
-        <span>Hello, world!</span>
-        <DoorMonitor />
+        <DoorMonitors />
     </div>
 ), document.body);
-
-
-window.onload = function () {
-    var conn;
-    if (window["WebSocket"]) {
-        conn = new WebSocket("ws://" + process.env.API_BASE + "/sub");
-        conn.onclose = function (evt) {
-            var item = document.createElement("div");
-            item.innerHTML = "<b>Connection closed.</b>";
-            appendLog(item);
-        };
-        conn.onmessage = function (evt) {
-            var messages = evt.data.split('\n');
-            for (var i = 0; i < messages.length; i++) {
-                var item = document.createElement("div");
-                item.innerText = messages[i];
-                appendLog(item);
-            }
-        };
-    } else {
-        var item = document.createElement("div");
-        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-        appendLog(item);
-    }
-};
